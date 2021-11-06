9a172fd73653404684a6b3562b2be86f1cd8d83c
Karen <karennwaomonoh@gmail.com>
2021-11-06 14:36:42 +0100

    This code is automates the process of filling in a form which I created for the purpose of this test. The form accepts only one response per individual so requires the user to sign into their Gmail account for validation. The setback on this project is that Gmail stops any automated signin so I had to use my school email address and password which alos runs on gmail platform so that worked.

--- fill_form.py
+++ fill_form.py
@@ -1,51 +1,62 @@
 from selenium import webdriver
 import time
-driver = webdriver.Chrome("../chromedriver2.exe")
-form = driver.get("https://docs.google.com/forms/d/e/1FAIpQLSeqwWseSyRTrTq91FdT75Kt0Tm0bfou81TNZ8HG1HSl9uSNWg/viewform?usp=sf_link")
 
-time.sleep(2)
-signin_button = driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[3]/div[2]/span/span')
-signin_button.click()
+while True:
 
-time.sleep(1)
-email = "*****" #fill in your email address in place of the "*"
-verify_email = driver.find_element_by_xpath('//*[@id="identifierId"]')
-verify_email.send_keys(email)
 
-submit_email = driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/span')
-submit_email.click()
+    driver = webdriver.Chrome("../chromedriver2.exe")
+    form = driver.get("https://docs.google.com/forms/d/e/1FAIpQLSeqwWseSyRTrTq91FdT75Kt0Tm0bfou81TNZ8HG1HSl9uSNWg/viewform?usp=sf_link")
 
-time.sleep(2)
-password = "***" #fill in your password for your email  in place of the "*"
-enter_pass = driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')
-enter_pass.send_keys(password)
+    time.sleep(2)
+    signin_button = driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[3]/div[2]/span/span')
+    signin_button.click()
 
-pass_click = driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/span')
-pass_click.click()
 
-time.sleep(4)
+    time.sleep(1)
 
-firstname = "***" #fill in your firstname in place of the "*"
-first = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-first.send_keys(firstname)
+    email = "****"
+    verify_email = driver.find_element_by_xpath('//*[@id="identifierId"]')
+    verify_email.send_keys(email)
 
-lastname = "***" #fill in your last name in place of the "*"
-first = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-first.send_keys(lastname)
+    submit_email = driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/span')
+    submit_email.click()
 
-age = 23
-first = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-first.send_keys(age)
+    time.sleep(2)
+    password = "****"
+    enter_pass = driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input')
+    enter_pass.send_keys(password)
 
-first = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input')
-first.send_keys(email)
+    pass_click = driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button/span')
+    pass_click.click()
+    try:
+        time.sleep(6)
 
-click_button = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
-click_button.click()
+        firstname = "****"
+        first = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        first.send_keys(firstname)
 
-get_confirmation_div_text = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div[3]')
-confirm = get_confirmation_div_text.text
-if get_confirmation_div_text == "Your response has been recorded.":
-    print("Process Successful")
-else:
-    print("Not successful")
+        lastname = "****"
+        first = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        first.send_keys(lastname)
+
+        age = 23
+        first = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        first.send_keys(age)
+
+
+        first = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        first.send_keys(email)
+
+        click_button = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
+        click_button.click()
+
+
+        get_confirmation_div_text = driver.find_element_by_class_name('freebirdFormviewerViewResponseConfirmationMessage')
+
+        print(get_confirmation_div_text.text)
+        if get_confirmation_div_text.text == "Your response has been recorded.":
+            print("Process Successful")
+        break
+    except:
+        print(f"You've already responded previously with the email: {email}. You can't respond twice")
+        break
